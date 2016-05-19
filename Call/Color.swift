ed25469ad1f73e52f35671748edeebd2ed8d4904
谭帆帆 <ilumanxi@qq.com>
2016-05-19 11:42:54 +0800

    init

---
+++ Call/Color.swift
@@ -0,0 +1,31 @@
+//
+//  Color.swift
+//  Call
+//
+//  Created by 谭帆帆 on 16/5/19.
+//  Copyright © 2016年 风起兮. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func rgb(red: Int,green: Int, blue: Int) ->UIColor {
+        
+        func percentage(num: Int) ->CGFloat {
+            
+            return CGFloat(num) / 255.0
+        }
+        
+        return UIColor(red: percentage(red), green: percentage(green), blue: percentage(blue), alpha: 1.0)
+    }
+    
+}
+
+
+
+@IBDesignable
+extension UIView {
+    
+    
+}
